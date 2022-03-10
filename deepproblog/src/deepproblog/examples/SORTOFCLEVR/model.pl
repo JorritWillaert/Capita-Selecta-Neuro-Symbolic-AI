8157d6764bbbf4ade6a7272ba5e177652e887959
JorritWillaert <jorritwillaert98@gmail.com>
2022-03-10 19:17:29 +0100

    Update model.pl in order to work on 6x6 grids

--- deepproblog/src/deepproblog/examples/SORTOFCLEVR/model.pl
+++ deepproblog/src/deepproblog/examples/SORTOFCLEVR/model.pl
@@ -1,7 +1,7 @@
 % We know that there are two objects in a four grid space. Two shapes possible, two colors possible.
 % The green cnn detects the position, and the shape --> x. If x < size: Shape = rectangle. Else: shape = circle. (x modulo size = position)  
 
-width(4).
+width(6).
 size(N) :-
     width(Width),
     N is Width * Width.
@@ -9,19 +9,22 @@ states(N) :-
     size(Size),
     N is Size * 2.
 
+% [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]
 % [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 % [0, 1, 2, 3, 4, 5, 6, 7]
-nn(cnn_red,[X],Y_red, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]) :: detect_state(red, X, Y_red).
-nn(cnn_green,[X], Y_green, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]) :: detect_state(green, X, Y_green).
-nn(cnn_blue,[X],Y_blue, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]) :: detect_state(blue, X, Y_blue).
-nn(cnn_orange,[X], Y_orange, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]) :: detect_state(orange, X, Y_orange).
+nn(cnn_red,[X],Y_red, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]) :: detect_state(red, X, Y_red).
+nn(cnn_green,[X], Y_green, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]) :: detect_state(green, X, Y_green).
+nn(cnn_blue,[X],Y_blue, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]) :: detect_state(blue, X, Y_blue).
+nn(cnn_orange,[X], Y_orange, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]) :: detect_state(orange, X, Y_orange).
 
 detect_same_states(Shape, Img, Count) :-
     check_state(red, Shape, Img, Return_red),
     check_state(green, Shape, Img, Return_green),
     check_state(blue, Shape, Img, Return_blue),
     check_state(orange, Shape, Img, Return_orange),
-    Count is Return_red + Return_green + Return_blue + Return_orange.
+    check_state(grey, Shape, Img, Return_grey),
+    check_state(yellow, Shape, Img, Return_yellow),
+    Count is Return_red + Return_green + Return_blue + Return_orange + Return_grey + Return_yellow.
 
 check_state(Color, Shape, Img, Return) :-
     detect_state(Color, Img, Y_color),
@@ -64,6 +67,20 @@ orange_shape(Img, Rectangle) :-
     \+ shape_is_rectangle(Img, orange),
     Rectangle = 0.
 
+grey_shape(Img, Rectangle) :-
+    shape_is_rectangle(Img, grey),
+    Rectangle = 1.
+grey_shape(Img, Rectangle) :-
+    \+ shape_is_rectangle(Img, grey),
+    Rectangle = 0.
+
+yellow_shape(Img, Rectangle) :-
+    shape_is_rectangle(Img, yellow),
+    Rectangle = 1.
+yellow_shape(Img, Rectangle) :-
+    \+ shape_is_rectangle(Img, yellow),
+    Rectangle = 0.
+
 shape_is_rectangle(Img, Color) :-
     detect_state(Color, Img, Y_color),
     size(Size),
@@ -97,6 +114,20 @@ orange_horizontal_side(Img, Left) :-
     Left = 1.
 orange_horizontal_side(Img, Left) :-
     \+ position_is_left(Img, orange),
+    Left = 0.
+
+grey_horizontal_side(Img, Left) :-
+    position_is_left(Img, grey),
+    Left = 1.
+grey_horizontal_side(Img, Left) :-
+    \+ position_is_left(Img, grey),
+    Left = 0.
+
+yellow_horizontal_side(Img, Left) :-
+    position_is_left(Img, yellow),
+    Left = 1.
+yellow_horizontal_side(Img, Left) :-
+    \+ position_is_left(Img, yellow),
     Left = 0.
 
 position_is_left(Img, Color) :-
@@ -134,6 +165,20 @@ orange_vertical_side(Img, Bottom) :-
     \+ position_is_bottom(Img, orange),
     Bottom = 0.
 
+grey_vertical_side(Img, Bottom) :-
+    position_is_bottom(Img, grey),
+    Bottom = 1.
+grey_vertical_side(Img, Bottom) :-
+    \+ position_is_bottom(Img, grey),
+    Bottom = 0.
+
+yellow_vertical_side(Img, Bottom) :-
+    position_is_bottom(Img, yellow),
+    Bottom = 1.
+yellow_vertical_side(Img, Bottom) :-
+    \+ position_is_bottom(Img, yellow),
+    Bottom = 0.
+
 position_is_bottom(Img, Color) :-
     detect_state(Color, Img, Y_color),
     position(Y_color, Pos),
@@ -157,6 +202,14 @@ blue_number_of_shapes(Img, Count) :-
 orange_number_of_shapes(Img, Count) :-
     detect_state(orange, Img, Y_orange),
     to_shape(Y_orange, Shape),
+    detect_same_states(Shape, Img, Count).
+grey_number_of_shapes(Img, Count) :-
+    detect_state(grey, Img, Y_grey),
+    to_shape(Y_grey, Shape),
+    detect_same_states(Shape, Img, Count).
+yellow_number_of_shapes(Img, Count) :-
+    detect_state(yellow, Img, Y_yellow),
+    to_shape(Y_yellow, Shape),    
     detect_same_states(Shape, Img, Count).
 
 to_shape(Y_color, Shape) :-
--- deepproblog/src/deepproblog/examples/SORTOFCLEVR/sort_of_clevr.py
+++ deepproblog/src/deepproblog/examples/SORTOFCLEVR/sort_of_clevr.py
@@ -15,7 +15,7 @@ import random
 random.seed(0)
 #np.random.seed(args.seed)
 
-width = 4
+width = 6
 out_size = (width ** 2) * 2 # Times two for the distinction between squares and circles
 
 colors = ["red", "green", "blue", "orange", "grey", "yellow"]
